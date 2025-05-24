8d4717b17911a3a275cff1e52bff5a8f0ed16a41
christianwolird <christian.wolird@gmail.com>
2025-05-23 20:34:40 -0700

    Read in previous terms

--- generate.py
+++ generate.py
@@ -1,1 +1,11 @@
-print('Where am I?')
+# Load previously calculated terms.
+with open("sequence.txt") as seq_file:
+    seq = [int(line.strip()) for line in seq_file]
+
+# Load all arithmetic progressions into a set.
+used_values = set()
+for n, a in enumerate(seq, start=1):
+    used_values.update(range(a, a + n**2, n))
+
+n = len(seq) + 1
+print(n, used_values)
